8a603b3443c054e70dcd423e121969294755e308
Catofes <i@catofes.com>
2016-03-28 21:20:51 +0800

    first release

--- server.cc
+++ server.cc
@@ -3,14 +3,68 @@
 //
 
 #include <iostream>
+#include <getopt.h>
+#include <sstream>
+#include <string>
 #include "epoll.hh"
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
+    string local_address = "";
+    string remote_address = "";
+    uint16_t local_port = 0;
+    uint16_t remote_port = 0;
+    extern char *optarg;
+    std::stringstream ss;
+    while (true) {
+        const int option = getopt(argc, argv, "a:b:c:d:e");
+        if (option == -1) break;
+        switch (option) {
+            case 'a':
+                ss.clear();
+                ss.str(optarg);
+                ss >> local_address;
+                break;
+            case 'b':
+                ss.clear();
+                ss.str(optarg);
+                ss >> local_port;
+                break;
+            case 'c':
+                ss.clear();
+                ss.str(optarg);
+                ss >> remote_address;
+                break;
+            case 'd':
+                ss.clear();
+                ss.str(optarg);
+                ss >> remote_port;
+                break;
+            case 'e':
+                break;
+        }
+    }
+
+    if (argc < 5) {
+        cout << " -a listen_address\n -b listen_port\n -c remote_address\n -d remote_port" << endl;
+        exit(0);
+    }
+    string str = "Listen at ";
+    str += local_address;
+    str += ":";
+    str += to_string(local_port);
+    Log::Log(str, 5);
+
+    str = "Connect to ";
+    str += remote_address;
+    str += ":";
+    str += to_string(remote_port);
+    Log::Log(str, 5);
+
     UEpoll epoll;
-    epoll.InitServer("0.0.0.0", 9000);
-    epoll.SetDestination("127.0.0.1", 9001);
+    epoll.InitServer(local_address, local_port);
+    epoll.SetDestination(remote_address, remote_port);
     epoll.Loop();
 }
